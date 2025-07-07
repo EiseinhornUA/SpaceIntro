92015b8eba47667ebbf848eef03b70e7d756672b
Daniel Beliaiev <daniel.beliaiev@gmail.com>
2025-07-07 17:18:41 +0100

    adjusted character light and removed player slow motion movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -21,15 +21,12 @@ public class PlayerController : MonoBehaviour
         animator = GetComponent<CharacterLoader>().GetAnimator();
         if (!animator) return;
 
-        Vector2 input = new Vector2(joystick.Horizontal, joystick.Vertical);
-
-        Debuger debuger = GameObject.FindAnyObjectByType<Debuger>();
-        if (debuger) debuger.ShowDebugText(input.ToString());
+        Vector2 inputDirection = new Vector2(joystick.Horizontal, joystick.Vertical);
 
         rotator.RotatePlayer(movement.GetCurrentVelocity().normalized);
 
         if (animator) animator.SetFloat("Speed", movement.GetCurrentSpeed());
 
-        movement.Move(input);
+        movement.Move(inputDirection.normalized);
     }
 }
