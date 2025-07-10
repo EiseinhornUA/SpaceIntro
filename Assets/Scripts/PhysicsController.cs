c2789a33e182d209a391c2b493ce3b79d594fbaa
Daniel Beliaiev <daniel.beliaiev@gmail.com>
2025-07-10 20:25:46 +0100

    added slop climbing
    without slop descending

--- Assets/Scripts/PhysicsController.cs
+++ Assets/Scripts/PhysicsController.cs
@@ -5,22 +5,23 @@ using System.Linq;
 using Unity.Burst.CompilerServices;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Timeline;
 
 [RequireComponent(typeof(BoxCollider2D))]
 public class PhysicsController : MonoBehaviour
 {
     [Header("Physics")]
     [SerializeField] private LayerMask collisionMask;
-    [SerializeField] private float gravity = -80;
+    [SerializeField] private float gravity = -80f;
     [SerializeField] private float friction = 0.01f;
     [SerializeField] private float airFriction = 0.01f;
     [SerializeField] private float maxVelocity = 16f;
-    [SerializeField] private float minVelocity = 0.01f;
+    [SerializeField] private float maxClimbAngle = 40f;
 
     [Header("Raycast Settings")]
     [SerializeField] int horizontalRayCount = 3;
     [SerializeField] int verticalRayCount = 3;
-    private const float skinWidth = 0.02f;
+    private const float skinWidth = 0.1f;
     private float horizontalRaySpacing;
     private float verticalRaySpacing;
     private BoxCollider2D boxCollider;
@@ -31,109 +32,109 @@ public class PhysicsController : MonoBehaviour
 
     private void Start()
     {
+        //Time.timeScale = 0.5f;
         boxCollider = GetComponent<BoxCollider2D>();
         CalculateRaySpacing();
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
         UpdatePhysics();
     }
 
-    public void UpdatePhysics()
+    private void UpdatePhysics()
     {
         UpdateRaycastOrigins();
         collisions.Reset();
+        
+        AddGravity();
+        AddForces();
 
-        velocity += Vector2.up * gravity * Time.deltaTime;
+        LimitVelocity();
 
+        Vector2 shift = velocity * Time.deltaTime;
+
+        ClimbSlope(ref shift);
+        CollideHorizontally(ref shift);
+        CollideVertically(ref shift);
+
+        transform.Translate(shift);
+
+        AddGroundFriction();
+        AddAirFriction();
+    }
+
+    private void AddGravity() => velocity += gravity * Time.deltaTime * Vector2.up;
+
+    private void AddForces()
+    {
         foreach (var force in forces)
         {
             velocity += force * Time.deltaTime;
         }
+    }
 
-        // Max speed limit
-        if(Mathf.Abs(velocity.magnitude) > maxVelocity)
+    private void LimitVelocity()
+    {
+        if (Mathf.Abs(velocity.magnitude) > maxVelocity)
         {
             velocity = velocity.normalized * maxVelocity;
         }
-        // Min speed limit
-        if(Mathf.Abs(velocity.x) < minVelocity)
-        {
-            velocity.x = 0;
-        }
-        if (Mathf.Abs(velocity.y) < minVelocity)
-        {
-            velocity.y = 0;
-        }
+    }
 
-        Vector2 shift = velocity * Time.deltaTime;
-
-        CollisionClampHorizontalShift(ref shift);
-        CollisionClampVerticalShift(ref shift);
-
-        transform.Translate(shift);
-        
-        // Ground friction
-        if(collisions.below || collisions.above)
-        {
-            velocity *= (1 - friction);
-        }
-        // Air friction
-        if(!collisions.below && !collisions.above)
+    private void AddAirFriction()
+    {
+        if (!collisions.below && !collisions.above)
         {
             velocity *= (1 - airFriction);
         }
     }
 
-    internal void SetGravity(float force)
+    private void AddGroundFriction()
     {
-        gravity = force;
-    }
-    internal float GetGravity()
-    {
-        return gravity;
-    }
-
-    public void AddForce(Vector2 force)
-    {
-        forces.Add(force);
+        if (collisions.below || collisions.above)
+        {
+            velocity *= (1 - friction);
+        }
     }
 
-    public void RemoveForce(Vector2 force)
+    internal void SetGravity(float force) => gravity = force;
+    internal float GetGravity() => gravity;
+
+    internal void AddForce(Vector2 force) => forces.Add(force);
+    internal void RemoveForce(Vector2 force) => forces.Remove(force);
+
+    internal void AddImpulse(Vector2 impuse) => velocity += impuse;
+
+    internal void SetVelocityX(float velocityX) => this.velocity.x = velocityX;
+    internal void SetVelocityY(float velocityY) => this.velocity.y = velocityY;
+
+    private void CollideHorizontally(ref Vector2 shift)
     {
-        forces.Remove(force);
+        List<RaycastHit2D> hits = GetHorizontalRaysHits(shift).Where(h => (bool)h).ToList();
+
+        if (hits.Count == 0) return;
+
+        float minDistance = hits.Select(h => h.distance).Min();
+
+        shift.x = Mathf.Sign(shift.x) * (minDistance - skinWidth);
+        //shift.x = 0;
+
+        velocity.x = 0;
     }
 
-    public void AddImpulse(Vector2 impuse)
-    {
-        velocity += impuse;
-    }
-
-    public void SetVelocityX(float velocityX) => this.velocity.x = velocityX;
-    public void SetVelocityY(float velocityY) => this.velocity.y = velocityY;
-
-    public void ClampVelocityX(float limit)
-    {
-        velocity.x = Mathf.Clamp(velocity.x, -limit, limit);
-    }
-
-    private void CollisionClampVerticalShift(ref Vector2 shift)
+    private void CollideVertically(ref Vector2 shift)
     {
         List<RaycastHit2D> hits = GetVerticalRaysHits(shift).ToList();
-        if (hits.Count == 0) return;
-        var minDistance = hits.Select(h => h.distance).Min();
-        shift.y = Mathf.Sign(shift.y) * (minDistance - skinWidth);
-        velocity.y = 0;
-    }
 
-    private void CollisionClampHorizontalShift(ref Vector2 shift)
-    {
-        List<RaycastHit2D> hits = GetHorizontalRaysHits(shift).ToList();
         if (hits.Count == 0) return;
-        var minDistance = hits.Select(h => h.distance).Min();
-        shift.x = Mathf.Sign(shift.x) * (minDistance - skinWidth);
-        velocity.x = 0;
+
+        float minDistance = hits.Select(h => h.distance).Min();
+
+        shift.y = Mathf.Sign(shift.y) * (minDistance - skinWidth);
+        //shift.y = 0;
+
+        velocity.y = 0;
     }
 
     private IEnumerable<RaycastHit2D> GetVerticalRaysHits(Vector2 shift)
@@ -164,6 +165,8 @@ public class PhysicsController : MonoBehaviour
         float directionX = Mathf.Sign(shift.x);
         float rayLength = Mathf.Abs(shift.x) + skinWidth;
 
+        List<RaycastHit2D> hits = new List<RaycastHit2D>();
+
         for (int i = 0; i < horizontalRayCount; i++)
         {
             Vector2 rayOrigin = (directionX < 0) ? raycastOrigins.bottomLeft : raycastOrigins.bottomRight;
@@ -172,14 +175,30 @@ public class PhysicsController : MonoBehaviour
             RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.right * directionX, rayLength, collisionMask);
 
             Debug.DrawRay(rayOrigin, Vector2.right * rayLength * directionX, Color.red);
-
+            
+            yield return hit;
             if (hit)
             {
-                yield return hit;
                 collisions.left = directionX == -1;
                 collisions.right = directionX == 1;
             }
         }
+    }
+
+    private void ClimbSlope(ref Vector2 shift)
+    {
+        var hits = GetHorizontalRaysHits(shift);
+
+        if (!hits.First()) return;
+
+        float slopeAngle = Vector2.Angle(hits.First().normal, Vector2.up);
+
+        if (slopeAngle > maxClimbAngle) return;
+
+        float moveDistance = Mathf.Abs(shift.x);
+        
+        shift.y = Mathf.Sin(slopeAngle * Mathf.Deg2Rad) * moveDistance;
+        shift.x = Mathf.Cos(slopeAngle * Mathf.Deg2Rad) * moveDistance * Mathf.Sign(shift.x);
     }
 
     private void UpdateRaycastOrigins()
@@ -187,8 +206,8 @@ public class PhysicsController : MonoBehaviour
         Bounds bounds = boxCollider.bounds;
         bounds.Expand(skinWidth * -2);
 
-        raycastOrigins.topLeft = new Vector2(bounds.min.x, bounds.max.y);
-        raycastOrigins.topRight = new Vector2(bounds.max.x, bounds.max.y);
+        raycastOrigins.topLeft =  new Vector2(bounds.min.x, bounds.max.y);
+        raycastOrigins.topRight =  new Vector2(bounds.max.x, bounds.max.y);
         raycastOrigins.bottomLeft = new Vector2(bounds.min.x, bounds.min.y);
         raycastOrigins.bottomRight = new Vector2(bounds.max.x, bounds.min.y);
     }
@@ -205,21 +224,13 @@ public class PhysicsController : MonoBehaviour
         verticalRaySpacing = bounds.size.x / (verticalRayCount - 1);
     }
 
-    internal LayerMask GetCollisionMask()
-    {
-        return collisionMask;
-    }
+    internal LayerMask GetCollisionMask() => collisionMask;
 
     public struct RaycastOrigins
     {
         public Vector2 topLeft, topRight;
         public Vector2 bottomLeft, bottomRight;
     }
-
-    // public List<> GetRaycastOrigins()
-    // {
-    //     return RaycastOrigins.top
-    // }
 
     public struct CollisionInfo
     {
