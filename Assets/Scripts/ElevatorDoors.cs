a682e8b04fe228e0af2f906e8a3966267b9a8523
Stas2133 <beliaiev.stas@gmail.com>
2025-07-17 19:34:03 +0100

    Organized Objects in scene, reneamed Elevator script

--- Assets/Scripts/ElevatorDoors.cs
+++ Assets/Scripts/ElevatorDoors.cs
@@ -4,7 +4,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ElevatorMechanics : MonoBehaviour
+public class ElevatorDoors : MonoBehaviour
 {
 
     [SerializeField]
