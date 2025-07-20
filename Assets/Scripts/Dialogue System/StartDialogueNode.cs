5aa0dd3acd087b6a9f064bf0cc4222405867a03e
Stas2133 <beliaiev.stas@gmail.com>
2025-07-20 19:03:00 +0100

    Fixed lift, changed robot, added background and space suit helmet

--- Assets/Scripts/Dialogue System/StartDialogueNode.cs
+++ Assets/Scripts/Dialogue System/StartDialogueNode.cs
@@ -20,7 +20,7 @@ public class StartDialogueNode : WaitUnit
 
     protected override void Definition()
     {
-        dialogueInput = ValueInput<DialoguePrefab>("Dialogue Prefab", new DialoguePrefab());
+        dialogueInput = ValueInput<DialoguePrefab>("Dialogue Prefab", default);
 
         enter = ControlInputCoroutine("enter", Await);
 
