0424bb318a19fedd0d7e18ee46dd8e2de50a8c03
Daniel Beliaiev <daniel.beliaiev@gmail.com>
2025-07-22 13:07:16 +0100

    fixed dialogue branching system

--- Assets/Scripts/OnInteractNode.cs
+++ Assets/Scripts/OnInteractNode.cs
@@ -8,6 +8,7 @@ using Cysharp.Threading.Tasks;
 [UnitCategory("Quest")]
 public class OnInteractNode : WaitUnit
 {
+    private const string TriggerName = "Trigger";
     [UnitHeaderInspectable("Interactable Count")]
     [Range(1, 10)]
     public int interactableCount = 1;
@@ -40,38 +41,54 @@ public class OnInteractNode : WaitUnit
 
     protected override IEnumerator Await(Flow flow)
     {
-        List<Interactable> interactables = new List<Interactable>();
-        for (int i = 0; i < interactableCount; i++)
+        List<Interactable> interactables = GetInteractables(flow);
+        foreach (var interactable in gameObjects)
         {
-            var go = flow.GetValue<GameObject>(gameObjects[i]);
-
-            GameObject child;
-            bool hasInteractable = false;
-            if (go.transform.childCount > 0)
-            {
-                child = go.transform.GetChild(0).gameObject;
-                hasInteractable = child.TryGetComponent<Interactable>(out Interactable interactable);
-                if (hasInteractable)
-                {
-                    interactable.Activate();
-                    interactables.Add(interactable);
-                }
-            }
-            if (!hasInteractable)
-            {
-                Interactable interactable;
-                child = new GameObject("Child");
-                child.transform.SetParent(go.transform, false);
-                interactable = child.AddComponent<Interactable>();
-                interactable.Activate();
-                interactables.Add(interactable);
-            }
+            Debug.Log("Waiting for interaction with" + flow.GetValue<GameObject>(interactable));
         }
+
         int index = 0;
         yield return UniTask.WhenAny(interactables.Select(i => i.WaitForInteraction())).ContinueWith(i => index = i).ToCoroutine();
         Debug.Log($"Interaction completed {index}");
         interactables.ForEach(i => i.Deactivate());
         yield return exits[index];
         Debug.Log($"Interaction exited {index}");
+    }
+
+    private List<Interactable> GetInteractables(Flow flow)
+    {
+        List<Interactable> interactables = new List<Interactable>();
+        for (int i = 0; i < interactableCount; i++)
+        {
+            var go = flow.GetValue<GameObject>(gameObjects[i]);
+
+            GameObject trigger;
+            bool hasInteractable = false;
+            if (go.transform.childCount > 0)
+            {
+                Transform transform = go.transform.Find(TriggerName);
+                if (transform)
+                {
+                    trigger = transform.gameObject;
+                    hasInteractable = trigger.TryGetComponent<Interactable>(out Interactable interactable);
+                    if (hasInteractable)
+                    {
+                        interactable.Activate();
+                        interactables.Add(interactable);
+                    }
+                }
+            }
+            if (!hasInteractable)
+            {
+                Interactable interactable;
+                trigger = new GameObject(TriggerName);
+                trigger.transform.SetParent(go.transform, false);
+                interactable = trigger.AddComponent<Interactable>();
+                interactable.Activate();
+                interactables.Add(interactable);
+            }
+        }
+
+        return interactables;
     }
 }
