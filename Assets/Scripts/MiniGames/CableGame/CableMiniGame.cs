6291722b63bc21523538a7af5e1e71b4b7bb1715
Daniel Beliaiev <daniel.beliaiev@gmail.com>
2025-07-24 14:35:59 +0100

    cable mini game wip

--- Assets/Scripts/MiniGames/CableGame/CableMiniGame.cs
+++ Assets/Scripts/MiniGames/CableGame/CableMiniGame.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CableMiniGame : MonoBehaviour
 {
@@ -10,5 +11,21 @@ public class CableMiniGame : MonoBehaviour
     private Wire GetWire(Vector2Int position)
     {
         return wireSlots[position.x + (position.y * size)].GetWire();
+    }
+
+    internal void DisableRaycasts()
+    {
+        foreach (WireSlot slot in wireSlots)
+        {
+            slot.GetWire().DisableRaycast();
+        }
+    }
+
+    internal void EnableRaycasts()
+    {
+        foreach (WireSlot slot in wireSlots)
+        {
+            slot.GetWire().EnableRaycast();
+        }
     }
 }
