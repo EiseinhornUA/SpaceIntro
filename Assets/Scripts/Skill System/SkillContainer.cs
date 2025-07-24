49ec0ae5842f730f48e22b4e75b3cfe785381b1d
Daniel Beliaiev <daniel.beliaiev@gmail.com>
2025-07-24 21:29:51 +0100

    added skill system

---
+++ Assets/Scripts/Skill System/SkillContainer.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class SkillContainer : MonoBehaviour
+{
+    [SerializeField] private List<SkillSO> skillSos;
+    private List<Skill> skills;
+
+    private void Start()
+    {
+        skills = skillSos.Select(skillSO => skillSO.AsSkill()).ToList();
+    }
+    private Skill GetSkill(SkillSO skill)
+    {
+        return skills.Find(s => s.skillName == skill.GetName());
+    }
+
+    public void AddSkillLevel(SkillSO skill, int amount)
+    {
+        GetSkill(skill).level += amount;
+    }
+
+    public void SubtractSkillLevel(SkillSO skill, int amount)
+    {
+        GetSkill(skill).level -= amount;
+    }
+
+    [ContextMenu("Print Skills")]
+    public void PrintSkills()
+    {
+
+        Debug.Log(string.Join('\n', skills.Select(skill => $"{skill.skillName}: {skill.level}")));
+        //foreach (var skill in skills)
+        //{
+        //    Debug.Log($"Skill: {skill.skillName}, Level: {skill.level}");
+        //}
+    }
+}
