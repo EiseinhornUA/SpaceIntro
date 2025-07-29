912c84bb0109d1f9a58d40040699d1be70d3c113
Daniel Beliaiev <daniel.beliaiev@gmail.com>
2025-07-29 16:51:26 +0100

    added logic to current task

---
+++ Assets/Scripts/InventorySystem/SetCurrentTaskNode.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using Unity.VisualScripting;
+using UnityEngine;
+
+
+[UnitCategory("Inventory System")]
+[UnitTitle("Set Current Task")]
+public class SetCurrentTaskNode : Unit
+{
+    public ControlInput enter;
+    public ControlOutput exit;
+    public ValueInput inputText;
+
+    protected override void Definition()
+    {
+        enter = ControlInput("enter", (flow) =>
+        {
+            InventoryView inventoryView = GameObject.FindObjectOfType<InventoryView>(includeInactive: true);
+            string taskText = flow.GetValue<string>(inputText);
+            if (!inventoryView)
+            {
+                throw new System.Exception("InventoryView not found in the scene.");
+            }
+            inventoryView.SetTaskText(taskText);
+            return exit;
+        });
+        exit = ControlOutput("exit");
+        inputText = ValueInput<string>("inputText", string.Empty);
+
+        Succession(enter, exit);
+        Requirement(inputText, enter);
+    }
+}
