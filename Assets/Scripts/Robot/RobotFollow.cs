eee96742c1838c0ae80c3bac6d6f6173d422e864
Stas2133 <beliaiev.stas@gmail.com>
2025-08-01 18:26:47 +0100

    Added flying robot WIP, orginized scripts

---
+++ Assets/Scripts/Robot/RobotFollow.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class RobotFollow : MonoBehaviour
+{
+    private const float ApproachingThreshold = 0.1f;
+    [SerializeField] private Transform player;
+    [SerializeField] private float followDistance = 2.5f;
+    [SerializeField] private float followSpeed = 10f;
+    [SerializeField] private float offsetX = 0.0f;
+    [SerializeField] private float offsetY = 1.5f;
+    [SerializeField] private float offsetZ = -0.5f;
+    [SerializeField] private float frequency = 0.5f; // Frequency of the idle movement
+    [SerializeField] private float amplitude = 0.01f; // Speed of the idle movement
+    private bool isRobotOn = false;
+    private float yzSpeedNormalizer = 0.2f;
+
+    private void Update()
+    {
+        if (isRobotOn)
+        {
+            FollowPlayer();
+            ApplyIdleMovement();
+            RotateTowardsPlayer();
+        }
+    }
+
+    public void TurnOnRobot()
+    {
+        isRobotOn = true;
+    }
+
+    private void RotateTowardsPlayer()
+    {
+        transform.LookAt(player.position + new Vector3(0f, offsetY, 1f));
+    }
+
+    private void ApplyIdleMovement()
+    {
+        transform.position += new Vector3(
+            0f, // Small horizontal movement
+            Mathf.Cos(Time.fixedTime * 2f * (float)Math.PI * frequency) * amplitude,
+            0f); // No movement in the Z direction
+    }
+
+    public void FollowPlayer()
+    {
+        Vector3 robotPosition = transform.position;
+
+        float directionX = player.position.x - transform.position.x + offsetX;
+        float directionY = player.position.y - transform.position.y + offsetY;
+        float directionZ = player.position.z - transform.position.z + offsetZ;
+
+        if (Mathf.Abs(directionX) > followDistance)
+        {
+            robotPosition.x += (Mathf.Sign(directionX) - ((followDistance - ApproachingThreshold) / directionX))
+                * followSpeed * Time.deltaTime;
+        }
+
+        robotPosition.y += yzSpeedNormalizer * directionY * followSpeed * Time.deltaTime;
+        robotPosition.z += yzSpeedNormalizer * directionZ * followSpeed * Time.deltaTime;
+        transform.position = robotPosition;
+    }
+}
