8bf770e46f51b23a3ea179c825a5d88074156c8c
Daniel Beliaiev <daniel.beliaiev@gmail.com>
2025-08-01 23:03:37 +0100

    added ability to change hair color
    fixed animations

---
+++ Assets/Scripts/CharacterSelection/HairColorContainerSO.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "HairColors", menuName = "ScriptableObjects/CharacterSelection/HairColors")]
+public class HairColorContainerSO : ScriptableObject
+{
+    [field: SerializeField] public List<Color> colors {  get; private set; }
+
+    public Color GetColor(int index) => colors[index];
+}
