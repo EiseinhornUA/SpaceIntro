db3363e3b04e79c16d4f4904c00f2c9d3dd144af
Stas2133 <beliaiev.stas@gmail.com>
2025-08-04 20:03:57 +0100

    Fixed FadeOut

--- Assets/Scripts/FadeOut.cs
+++ Assets/Scripts/FadeOut.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -6,48 +7,15 @@ public class FadeOut : MonoBehaviour
     [SerializeField] private Image fadeImage;
     [SerializeField] private float fadeSpeed = 1f;
 
-    private bool enableFadeOut = true;
-
-    void Update()
-    {
-        Color color = fadeImage.color;
-        float alphaChange = fadeSpeed * Time.deltaTime;
-
-        if (enableFadeOut && color.a > 0f)
-        {
-            color.a -= alphaChange;
-            color.a = Mathf.Clamp01(color.a);
-            fadeImage.color = color;
-        }
-        else if (!enableFadeOut && color.a < 1f)
-        {
-            color.a += alphaChange;
-            color.a = Mathf.Clamp01(color.a);
-            fadeImage.color = color;
-        }
-    }
-
-    public void EnableFadeOut()
-    {
-        enableFadeOut = true;
-    }
-
-    public void EnableFadeIn()
-    {
-        enableFadeOut = false;
-    }
-
     [ContextMenu("Show Fade Out")]
     public void ShowFadeOut()
     {
-        fadeImage.color = new Color(1f, 1f, 1f, 1f);
-        EnableFadeOut();
+        fadeImage.DOFade(1f, fadeSpeed);
     }
 
     [ContextMenu("Show Fade In")]
     public void ShowFadeIn()
     {
-        fadeImage.color = new Color(1f, 1f, 1f, 0f);
-        EnableFadeIn();
+        fadeImage.DOFade(0f, fadeSpeed);
     }
 }
