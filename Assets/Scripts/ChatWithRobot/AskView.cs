bb2736b666890f47f96a58a6dbae99a34a3d3996
Daniel Beliaiev <daniel.beliaiev@gmail.com>
2025-08-12 20:06:01 +0100

    implemented chatbot

---
+++ Assets/Scripts/ChatWithRobot/AskView.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class AskView : MonoBehaviour
+{
+    [SerializeField] private TMP_InputField inputField;
+    [field: SerializeField] public Button askButton { get; private set;}
+    [SerializeField] private TextMeshProUGUI responeText;
+    [SerializeField] private Button closeButton;
+    [SerializeField] private Hud hud;
+
+    private void Start()
+    {
+        closeButton.onClick.AddListener(Hide);
+    }
+
+    [ContextMenu("Show")]
+    public void Show()
+    {
+        gameObject.SetActive(true);
+        hud.HideHud();
+    }
+
+    public void Hide()
+    {
+        gameObject.SetActive(false);
+        hud.ShowHud();
+    }
+
+    public void SetResponse(string response) => responeText.text = response;
+
+    public void ClearInput() => inputField.text = string.Empty;
+
+    public string GetQuestionText() => inputField.text.Trim();
+}
---
+++ Assets/Scripts/ChatWithRobot/RobotChat.cs
@@ -0,0 +1,87 @@
+﻿using Cysharp.Threading.Tasks;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(AskView))]
+public class RobotChat : MonoBehaviour
+{
+    private const string apiUrl = "https://e-spaceintroai-chatbot.onrender.com/chat";
+    private List<string> conversationHistory = new List<string>();
+
+    private AskView askView;
+
+    private void Start()
+    {
+        askView = GetComponent<AskView>();
+        askView.askButton.onClick.AddListener(OnAskButtonClicked);
+    }
+
+    private void OnAskButtonClicked()
+    {
+        string question = askView.GetQuestionText();
+        if (!string.IsNullOrEmpty(question))
+        {
+            SendChatRequest(question);
+        }
+    }
+
+    private async UniTask SendChatRequest(string question)
+    {
+        ChatRequest requestData = new ChatRequest
+        {
+            user_id = PlayerPrefs.GetString("CharacterName", "Student"),
+            question = question,
+            conversation_history = conversationHistory
+        };
+
+        string json = JsonUtility.ToJson(requestData);
+        byte[] bodyRaw = Encoding.UTF8.GetBytes(json);
+
+        UnityWebRequest request = new UnityWebRequest(apiUrl, "POST");
+        request.uploadHandler = new UploadHandlerRaw(bodyRaw);
+        request.downloadHandler = new DownloadHandlerBuffer();
+        request.SetRequestHeader("Content-Type", "application/json");
+
+        askView.SetResponse("Thinking...");
+
+        await request.SendWebRequest();
+
+        if (request.result == UnityWebRequest.Result.Success)
+        {
+            string jsonResponse = request.downloadHandler.text;
+            ChatResponse response = JsonUtility.FromJson<ChatResponse>(jsonResponse);
+
+            if (response != null && !string.IsNullOrEmpty(response.response))
+            {
+                askView.SetResponse(response.response);
+                conversationHistory.Add(question);
+                conversationHistory.Add(response.response);
+            }
+            else
+            {
+                askView.SetResponse("No response received from the server.");
+            }
+        }
+    }
+}
+
+[System.Serializable]
+public class ChatRequest
+{
+    public string user_id;
+    public string question;
+    public List<string> conversation_history;
+}
+
+[System.Serializable]
+public class ChatResponse
+{
+    public string response;
+    public string[] context_used;
+}
