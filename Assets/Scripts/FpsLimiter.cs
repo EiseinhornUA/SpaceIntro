3ba6a22efe371fdd9402e95652f930f868a19d58
Stas2133 <beliaiev.stas@gmail.com>
2025-08-15 14:52:31 +0100

    Added realtimeChange ability for FpsLimiter

--- Assets/Scripts/FpsLimiter.cs
+++ Assets/Scripts/FpsLimiter.cs
@@ -5,9 +5,18 @@ using UnityEngine;
 public class FpsLimiter : MonoBehaviour
 {
     [SerializeField] private int fps = 30;
+    [SerializeField] private bool realtimeChange = false;
 
     private void Awake()
     {
         Application.targetFrameRate = fps;
+    }
+
+
+    private void Update()
+    {
+        if (realtimeChange)
+            if (fps != Application.targetFrameRate)
+                Application.targetFrameRate = fps;
     }
 }
