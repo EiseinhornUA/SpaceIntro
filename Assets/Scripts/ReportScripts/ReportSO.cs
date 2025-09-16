fbb5724202653094086a772c38d9bb690158988a
Stas2133 <beliaiev.stas@gmail.com>
2025-09-16 21:20:32 +0100

    Added all reports to the game

---
+++ Assets/Scripts/ReportScripts/ReportSO.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "Report", menuName = "ScriptableObjects/Report")]
+public class ReportSO : ScriptableObject
+{
+    [SerializeField] private string reportName;
+    [TextArea(3, 15)]
+    [SerializeField] private string reportDescription;
+
+    public string GetName() => reportName;
+    public string GetDescription() => reportDescription;
+}
