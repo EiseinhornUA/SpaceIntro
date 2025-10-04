9408a2a918ad71e60a89b67aa5368e56323d12b6
Daniel Beliaiev <daniel.beliaiev@gmail.com>
2025-10-04 19:24:13 +0100

    fixed build compilation errors

--- Assets/Scripts/Database/DatabaseManager.cs
+++ Assets/Scripts/Database/DatabaseManager.cs
@@ -5,8 +5,6 @@ using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
-using UnityEditor.UI;
 using UnityEngine;
 
 public class DatabaseManager : MonoBehaviour
