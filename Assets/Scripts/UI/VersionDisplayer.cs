8a787c7176214f37ad3f01f9773c8c4396d9191a
Daniel Beliaiev <daniel.beliaiev@gmail.com>
2026-01-12 13:37:31 +0000

    added version displayer

--- Assets/Scripts/UI/VersionDisplayer.cs
+++ Assets/Scripts/UI/VersionDisplayer.cs
@@ -1,14 +1,11 @@
-using System.Collections;
-using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
+[RequireComponent(typeof(TextMeshProUGUI))]
 public class VersionDisplayer : MonoBehaviour
 {
-    [SerializeField] private TextMeshProUGUI textTMP;
-
     private void Start()
     {
-        textTMP.text = "Version " + Application.version;
+        GetComponent<TextMeshProUGUI>().text = "version: " + Application.version;
     }
 }
